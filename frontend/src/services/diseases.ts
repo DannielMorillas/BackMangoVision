cc4c74e6bcf3f1792c7c7a79b9a7aa3f52e37b89
Daniel Morillas <danielmorillas0@gmail.com>
2026-05-24 16:38:51 -0500

    split: mover frontend a FrontMangoVision (repo separado)

--- frontend/src/services/diseases.ts
+++
@@ -1,6 +0,0 @@
-import { apiGet } from "./api";
-import type { Disease } from "../types/disease";
-
-export function fetchDiseases(): Promise<Disease[]> {
-  return apiGet<Disease[]>("/diseases");
-}
--- frontend/src/types/disease.ts
+++
@@ -1,7 +0,0 @@
-export interface Disease {
-  id: number;
-  slug: string;
-  name: string;
-  color_hex: string;
-  description: string;
-}
